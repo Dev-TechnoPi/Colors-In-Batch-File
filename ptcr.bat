367d1a81d990cb8a26af952114e76d2cf5497b0f
Dev-TechnoPi <57293614+Dev-TechnoPi@users.noreply.github.com>
2019-11-04 17:06:19 +0700

    Ptcr.bat

--- ptcr.bat
+++ ptcr.bat
@@ -1,7 +1,7 @@
 @if defined show_commands (@echo on) else (@echo off) && setlocal enableextensions enabledelayedexpansion
 for %%A in (/h -h) do if /i "%~1" == "%%A" (
     echo.
-    echo Usage / Syntax : ptcr.bat [-H] [/H] ^<Text^> ^<Color^>
+        echo Usage / Syntax : ptcr.bat [-H] [/H] ^<Text^> ^<Color^>
 	echo Text     : To specify the text to print.
 	echo Color    : To specify the color to print.
 	echo -H or /H : Displays this help message.
@@ -21,12 +21,13 @@ for %%A in (/h -h) do if /i "%~1" == "%%A" (
 	echo Black, Dark Blue, Dark Green, Dark Cyan, DarkRed
 	echo Dark Magenta, Dark Yellow, Gray, Dark Gray, Blue
 	echo Green, Cyan, Red, Magenta, Yellow  White.
+	echo.
 	echo When using the dark colors, do not use spaces.
-	echo Put the two words together	like this : DarkGray.
+	echo Put the two words together. Example : DarkGray.
 	echo.
 	echo Infomation :
-	echo Created by : Kyle Vu 2017-2019.
-    echo Copyright (C) Kyle Vu 2019.
+    echo Created by : Kyle Vu, ^(C^) 2019.
+	echo Ptcr.bat is licensed by the MIT license.
 	endlocal && exit /b 0
     )
 if not "%~1" == "" set "text=%~1"
